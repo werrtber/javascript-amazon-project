891ccf6acadd87027a14468f166f2cc084db2143
Werrtber <werrtber16@gmail.com>
2025-08-11 22:06:58 +0300

    ось зараз баг

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) =>{
       matcingProduct = product;
     }
   })
-  cartProductsHtml += `<div class="cart-item-container">
+  cartProductsHtml += `<div class="cart-item-container js-cart-item-container-${matcingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -96,6 +96,7 @@ forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   })
 }) 
